b8ec5febc277e8345ddce989fafef2d8ae490dd4
Rony <ronybarua-ethical18>
2024-11-18 14:50:18 +0600

    modal closing issue solved

--- src/components/SharedDeleteActionCell.tsx
+++ src/components/SharedDeleteActionCell.tsx
@@ -1,35 +1,30 @@
+'use client';
+
 import { useState } from 'react';
-import { Button } from './ui/button';
 import { Trash2 } from 'lucide-react';
-import { cn } from '@/lib/utils';
 import SharedModal from './SharedModal';
 import DeleteConfirmationContent from './DeleteConfirmationContent';
 
 const SharedDeleteActionCell = ({
-  textVisible,
   itemId,
   itemOrigin,
 }: {
   itemId: string;
   itemOrigin: string;
-  textVisible?: boolean;
 }) => {
   const [isModalOpen, setModalOpen] = useState<boolean>(false);
-  console.log('__isOpen', isModalOpen);
 
   const handleDelete = () => {
     setModalOpen(true);
   };
 
   return (
-    <div className="flex items-center space-x-2">
-      <Button
-        onClick={handleDelete}
-        variant="ghost"
-        className={cn('h-8 w-full flex justify-start px-2')}
-      >
-        <Trash2 className="h-4 w-4 text-[#5B52F9] cursor-pointer" />
-        {textVisible && <span className="ms-2">Delete</span>}
+    <div className="flex items-center space-x-2 my-2">
+      <div>
+        <Trash2
+          className="h-4 w-4 text-[#5B52F9] cursor-pointer"
+          onClick={handleDelete}
+        />
         <div className="bg-white z-50">
           <SharedModal
             open={isModalOpen}
@@ -43,7 +38,7 @@ const SharedDeleteActionCell = ({
             />
           </SharedModal>
         </div>
-      </Button>
+      </div>
     </div>
   );
 };
