8210021885ab34d3ef81d1dbe87c98d811b576c1
Rony <ronybarua-ethical18>
2024-11-28 12:21:57 +0600

    expense overview section also dynamic for dashboard

--- src/components/layout/customer/components/dashboard/DashboardOverviewSection.tsx
+++ src/components/layout/customer/components/dashboard/DashboardOverviewSection.tsx
@@ -1,11 +1,25 @@
+'use client';
+
 import React from 'react';
 import MonthlyOverview from './MonthlyOverview';
 import YearlyExpenseTable from './YearlyExpenseTable';
+import { trpc } from '@/utils/trpc';
 
 const DashboardOverviewSection = () => {
+  const { data: expensesResponse } = trpc.expenses.getExpenses.useQuery(
+    {
+      page: 1,
+      limit: 5,
+    },
+    {
+      keepPreviousData: true,
+    }
+  );
+
+  console.log('expensesResponse', expensesResponse);
   return (
     <div className="grid grid-cols-12 gap-2 mt-2">
-      <YearlyExpenseTable />
+      <YearlyExpenseTable data={expensesResponse?.data || []} />
       <MonthlyOverview />
     </div>
   );
