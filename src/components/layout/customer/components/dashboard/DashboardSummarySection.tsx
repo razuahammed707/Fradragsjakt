bf3e35d684915651a28a72ec0ca4ad163123babf
zubaerSumon <zubaer@corexlab.dev>
2024-12-03 19:38:01 +0600

    dynamic_expense_card modified

--- src/components/layout/customer/components/dashboard/DashboardSummarySection.tsx
+++ src/components/layout/customer/components/dashboard/DashboardSummarySection.tsx
@@ -7,59 +7,12 @@ import YearlyExpenseGraph from './YearlyExpenseGraph';
 import { trpc } from '@/utils/trpc';
 //import { updateExpenses } from '@/utils/helpers/categoryMapperForExpense';
 import AggregatedExpenseCard from './AggregatedExpenseCard';
-const businessSpending = {
-  title: 'Business Spending (Total)',
-  total: 'NOK 6,150',
-  items: [
-    {
-      category: 'Office and Workspace',
-      amount: 'NOK 250',
-      difference: '+NOK 50',
-    },
-    {
-      category: 'Technology and Communication',
-      amount: 'NOK 250',
-      difference: '+NOK 50',
-    },
-    {
-      category: 'Professional Services',
-      amount: 'NOK 250',
-      difference: '-NOK 50',
-    },
-  ],
-};
+import { finalCalculation } from '@/utils/helpers/primaryCategoriesWithFormula';
+import { predefinedCategories } from '@/utils/dummy';
+import { useAppSelector } from '@/redux/hooks';
+import { questionnaireSelector } from '@/redux/slices/questionnaire';
+import { savingExpenseCalculator } from '@/utils/helpers/savingExpenseCalculator';
 
-const personalSpending = {
-  title: 'Personal Spending (Total)',
-  total: 'NOK 6,150',
-  items: [
-    {
-      category: 'Health & Family',
-      amount: 'NOK 300',
-      difference: '-NOK 50',
-    },
-    {
-      category: 'Banks & Loans',
-      amount: 'NOK 250',
-      difference: '+NOK 50',
-    },
-    {
-      category: 'Housing & Property',
-      amount: 'NOK 250',
-      difference: '-NOK 50',
-    },
-    {
-      category: 'Furniture and Equipment',
-      amount: 'NOK 250',
-      difference: '-NOK 50',
-    },
-    {
-      category: 'Office Supplies',
-      amount: 'NOK 350',
-      difference: '-NOK 50',
-    },
-  ],
-};
 const DashboardSummarySection = () => {
   const { data: expensesAnalytics } =
     trpc.expenses.getCategoryAndExpenseTypeWiseExpenses.useQuery({
@@ -67,6 +20,8 @@ const DashboardSummarySection = () => {
     });
 
   const categoryAnalytics = expensesAnalytics?.data?.categoryWiseExpenses;
+  const cardData = finalCalculation(categoryAnalytics, predefinedCategories);
+
   // const personalExpenses =
   //   expensesAnalytics?.data?.expenseTypeWiseExpenses?.find(
   //     (item: { expense_type: string }) => item.expense_type === 'personal'
@@ -76,6 +31,61 @@ const DashboardSummarySection = () => {
   //   expenses(personalExpenses?.amount),
   //   categoryAnalytics
   // );
+  const { questionnaires } = useAppSelector(questionnaireSelector);
+  const { data: user } = trpc.users.getUserByEmail.useQuery();
+  const {
+    workAndEducationExpenseAmount,
+    healthAndFamilyExpenseAmount,
+    bankAndLoansExpenseAmount,
+    hobbyOddjobsAndExtraIncomesExpenseAmount,
+    housingAndPropertyExpenseAmount,
+    giftsOrDonationsExpenseAmount,
+    foreignIncomeExpenseAmount,
+  } = savingExpenseCalculator(questionnaires, user?.questionnaires);
+  const personalData = [
+    {
+      title: 'Health and Family',
+      total_amount: healthAndFamilyExpenseAmount,
+      total_original_amount: 0,
+      predefinedCategories: [],
+    },
+    {
+      title: 'Bank and Loans',
+      total_amount: bankAndLoansExpenseAmount,
+      total_original_amount: 0,
+      predefinedCategories: [],
+    },
+    {
+      title: 'Work and Education',
+      total_amount: workAndEducationExpenseAmount,
+      total_original_amount: 0,
+      predefinedCategories: [],
+    },
+    {
+      title: 'Housing and Property',
+      total_amount: housingAndPropertyExpenseAmount,
+      total_original_amount: 0,
+      predefinedCategories: [],
+    },
+    {
+      title: 'Gifts/Donations',
+      total_amount: giftsOrDonationsExpenseAmount,
+      total_original_amount: 0,
+      predefinedCategories: [],
+    },
+    {
+      title: 'Hobby, Odd jobs, and Extra incomes',
+      total_amount: hobbyOddjobsAndExtraIncomesExpenseAmount,
+      total_original_amount: 0,
+      predefinedCategories: [],
+    },
+    {
+      title: 'Foreign Income',
+      total_amount: foreignIncomeExpenseAmount,
+      total_original_amount: 0,
+      predefinedCategories: [],
+    },
+  ];
 
   return (
     <div className="grid grid-cols-12 gap-2">
@@ -96,8 +106,15 @@ const DashboardSummarySection = () => {
         ))}
       </div> */}
       <div className="col-span-12 grid grid-cols-2 gap-2">
-        <AggregatedExpenseCard {...businessSpending} />
-        <AggregatedExpenseCard {...personalSpending} origin="personal" />
+        <AggregatedExpenseCard
+          items={cardData}
+          title="Tax saved from Business Spending (Total)"
+        />
+        <AggregatedExpenseCard
+          title="Tax saved from Personal Spending (Total)"
+          items={personalData}
+          origin="personal"
+        />
       </div>
     </div>
   );
--- src/utils/helpers/primaryCategoriesWithFormula.ts
+++ src/utils/helpers/primaryCategoriesWithFormula.ts
@@ -7,18 +7,22 @@ type DBCategories = {
 type PredefinedCategories = {
   id: number;
   name: string;
-  items: { name: string; amount: number }[];
+  items: { name: string; amount: number; original_amount: number }[];
   total_amount: number;
+  total_original_amount: number;
 };
 
 export const finalCalculation = (
   dbCategories: DBCategories[],
+
   predefinedCategories: PredefinedCategories[]
 ) => {
+  console.log({ dbCategories });
+
   return predefinedCategories.map((predefined: PredefinedCategories) => {
     const predefinedCategories = predefined.items.map(
-      (item: { name: string; amount: number }) => {
-        const matchedWithDbCategory = dbCategories.find(
+      (item: { name: string; amount: number; original_amount: number }) => {
+        const matchedWithDbCategory = dbCategories?.find(
           (dbCategory: DBCategories) => dbCategory.category === item.name
         );
         if (matchedWithDbCategory) {
@@ -26,17 +30,20 @@ export const finalCalculation = (
             return {
               ...item,
               amount: item.amount - matchedWithDbCategory.amount || 0,
+              original_amount: matchedWithDbCategory.amount || 0,
             };
           }
           if (matchedWithDbCategory.category === 'Computer Hardware') {
             return {
               ...item,
               amount: item.amount - matchedWithDbCategory.amount || 0,
+              original_amount: matchedWithDbCategory.amount || 0,
             };
           }
           return {
             ...item,
             amount: matchedWithDbCategory.amount,
+            original_amount: matchedWithDbCategory.amount,
           };
         }
         return item;
@@ -46,10 +53,18 @@ export const finalCalculation = (
       (acc: number, curr: { amount: number }) => acc + curr.amount,
       0
     );
+
+    const total_original_amount = predefinedCategories.reduce(
+      (acc: number, curr: { original_amount: number }) =>
+        acc + curr.original_amount,
+      0
+    );
+
     return {
       title: predefined.name,
       predefinedCategories,
       total_amount,
+      total_original_amount,
     };
   });
 };
