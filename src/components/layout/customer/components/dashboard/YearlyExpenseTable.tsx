d84df488acd59f36f3575160eb149b372190203b
sadat66 <imteaz.sadat@corexlab.dev>
2024-12-11 17:00:34 +0600

    added translation to Dashboard

--- src/components/layout/customer/components/dashboard/YearlyExpenseTable.tsx
+++ src/components/layout/customer/components/dashboard/YearlyExpenseTable.tsx
@@ -2,6 +2,7 @@
 import { SharedDataTable } from '@/components/SharedDataTable';
 import React from 'react';
 import { YearlyExpenseTableColumns } from './YearlyExpenseTableColumns';
+import { useTranslation } from '@/lib/TranslationProvider';
 
 // Define the type for a single expense item
 type YearlyExpenseItem = {
@@ -21,17 +22,18 @@ type YearlyExpenseTableProps = {
 };
 
 const YearlyExpenseTable = ({ data }: YearlyExpenseTableProps) => {
+  const { translate } = useTranslation();
   return (
     <div className="col-span-9 space-y-6 p-6 rounded-2xl bg-white">
       <div>
         <h4 className="text-sm text-[#101010] font-semibold">
-          Recent Expenses Overview
+          {translate('yearlyExpenseTable.title')}
         </h4>
       </div>
       <div className="">
         <SharedDataTable
           className="max-h-[250px]"
-          columns={YearlyExpenseTableColumns}
+          columns={YearlyExpenseTableColumns()}
           data={data || []}
         />
       </div>
