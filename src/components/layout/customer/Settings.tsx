261de51423ac557f440d67447157cacb6d40dd42
sadat66 <imteaz.sadat@corexlab.dev>
2024-12-17 15:59:43 +0600

    updated settings

--- src/components/layout/customer/Settings.tsx
+++ src/components/layout/customer/Settings.tsx
@@ -2,13 +2,11 @@
 
 import React from 'react';
 import ProtectedLayout from '../ProtectedLayout';
-import { AddAuditorModal } from './components/settings/AddAuditorModalContent';
 
 export default function CustomerSettings() {
   return (
     <ProtectedLayout>
       <div>Setting page</div>
-      <AddAuditorModal></AddAuditorModal>
     </ProtectedLayout>
   );
 }
