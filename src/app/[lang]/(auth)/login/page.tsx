4633161dfb941ae8d075bd945375984b083050e5
sadat66 <imteaz.sadat@corexlab.dev>
2025-01-06 18:44:01 +0600

    added metadata description to login page

--- src/app/[lang]/(auth)/login/page.tsx
+++ src/app/[lang]/(auth)/login/page.tsx
@@ -2,7 +2,8 @@ import Login from '@/components/layout/auth/Login';
 import React from 'react';
 
 export const metadata = {
-  title: 'Skattepluss Login Page',
+  title: 'Skattepluss | Login Page',
+  description: 'Login to Skattepluss, it is easy and secure',
 };
 const LoginPage = () => {
   return (
