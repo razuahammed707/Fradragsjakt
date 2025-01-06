5e4d908e3837850a1b5b8e4cf1c629f4b260c18c
sadat66 <imteaz.sadat@corexlab.dev>
2025-01-06 18:45:07 +0600

    description updated

--- src/app/[lang]/(auth)/forgot-password/page.tsx
+++ src/app/[lang]/(auth)/forgot-password/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ForgotPasswordComponent from '@/components/layout/auth/ForgotPassword';
 export const metadata = {
-  title: 'Skattepluss |Forgot Password',
+  title: 'Skattepluss | Forgot Password',
   description: 'Forgot password in Skattepluss? no worries, reset it here',
 };
 export default function ForgotPassword() {
