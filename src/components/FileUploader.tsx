eb5821b2dcf7ccf57fd2560fa80cfb73733d3a6e
sadat66 <imteaz.sadat@corexlab.dev>
2025-01-19 16:45:47 +0600

    redesigned the upload statement modal

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -44,11 +44,12 @@ export const FileUploader: React.FC<FileUploaderProps> = ({
 
   return (
     <div
-      className="rounded-lg mb-5 mt-2 bg-[#F0EFFE] p-5 border-dashed border-2 border-[#5B52F9]"
       {...getRootProps()}
+      className={`rounded-lg mb-5 mt-2 bg-[#FAFAFA] p-5 border-dashed border-2 border-[#D8D8D8] flex flex-col items-center justify-center ${
+        isDragActive ? 'bg-gray-100' : ''
+      }`}
     >
       <DragAndDropFile
-        type="spreadsheet"
         fileLink={null}
         loading={loading}
         getInputProps={getInputProps}
