b31141c45145898e32064e56f7e8d0d37767c1f8
zubaer_sumon <zubaer.rahman.cse@gmail.com>
2025-04-09 22:21:28 +0600

    new onboard questionnaires stepper added

---
+++ src/app/[lang]/(auth)/new-onboard/page.tsx
@@ -0,0 +1,17 @@
+import NewOnboard from '@/components/layout/auth/NewOnboard';
+import React from 'react';
+
+export const metadata = {
+  title: 'Skattepluss | New Onboarding ',
+  description: 'Get to know you better for personalized tax deductions',
+};
+
+const NewOnboardPage = () => {
+  return (
+    <>
+      <NewOnboard />
+    </>
+  );
+};
+
+export default NewOnboardPage;
